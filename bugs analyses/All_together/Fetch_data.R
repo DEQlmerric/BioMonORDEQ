20116c0678da68c6c051f44caa123948dc775195
Travis Pritchard <pritchard.travis@deq.state.or.us>
2024-05-10 11:20:03 -0700

    Another commit
    Adding leppo tax data to data fetch function
    Adding some flair to front end cause it's friday.

--- bugs analyses/All_together/Fetch_data.R
+++ bugs analyses/All_together/Fetch_data.R
@@ -2,7 +2,8 @@ library(tidyverse)
 library(StreamCatTools)
 
 
-fetch_data <- function(taxonomy_table = 'bugs analyses/Taxonomy/ODEQ_Taxonomy_dec22.xlsx'){
+fetch_data <- function(DEQ_taxonomy_table = 'bugs analyses/Taxonomy/ODEQ_Taxonomy_dec22.xlsx',
+                       leppo_taxonomy_table_url = 'https://raw.githubusercontent.com/leppott/BioMonTools_SupportFiles/main/data/taxa_official/ORWA_TaxaTranslator_20240204.csv'){
   
   
 
@@ -15,8 +16,13 @@ fetch_data <- function(taxonomy_table = 'bugs analyses/Taxonomy/ODEQ_Taxonomy_de
   
   ## Join Taxonomy table ---------------------------------------------------------------------------------------------
   
+  #AWQMS taxa = Taxonomic_Name
+  #Join DEQ taxonomy table by DEQ_Taxon to get Taxon
+  #Match Taxon to leppo's taxa table by Taxon = Taxon_orig
+  #Keep   OTU_RIV_24 from DEA taxa table and OTU_MTTI, OTU_BST from leppo taxa table
   
-  taxonomy <- readxl::read_excel(taxonomy_table, col_types = "text")
+  #read in DEQ taxa table
+  taxonomy <- readxl::read_excel(DEQ_taxonomy_table, col_types = "text")
   
   
   taxonomy.otu <- taxonomy %>%
@@ -25,10 +31,23 @@ fetch_data <- function(taxonomy_table = 'bugs analyses/Taxonomy/ODEQ_Taxonomy_de
   taxonomy.otu$DEQ_Taxon <- as.character(taxonomy.otu$DEQ_Taxon)
   
   
+  #read in leppo taxa table
+  taxonomy_leppo <- read.csv(leppo_taxonomy_table_url)
+ 
+  
+  
+   taxonomy_leppo_select <- taxonomy_leppo |> 
+    select(Taxon_orig,OTU_MTTI, OTU_BSTI) |> 
+    rename(Taxon = Taxon_orig)
+  
+  
+
+  
   raw.bugs_taxonomy <- raw_bugs %>%
-    left_join(taxonomy.otu, by='DEQ_Taxon')
+    left_join(taxonomy.otu, by='DEQ_Taxon') |> 
+    left_join(taxonomy_leppo_select, by = join_by(Taxon))
   
-  
+
 
   
   return(raw.bugs_taxonomy)

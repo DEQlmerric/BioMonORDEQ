fc57cdc3afb87854676fb352d09121aafaff9189
Travis Pritchard <pritchard.travis@deq.state.or.us>
2024-07-16 14:33:29 -0700

    another update

---
+++ bugs analyses/All_together/delete-EDA graphs_SHMMI.R
@@ -0,0 +1,286 @@
+
+# Start here ------------------------------------------------------------------------------------------------------
+
+#SH_MMI
+
+SH_MMI <- read.csv('C:/Users/tpritch/OneDrive - Oregon/R Projects/BioMonORDEQ/bugs analyses/MMI/_2024 model build/final_MMI_4.metrics.csv') |> 
+  select(SAMPLEID,MMI.2024)
+
+
+
+MMI <- SH_MMI |> 
+  rename(MMI = MMI.2024) |> 
+  select(SAMPLEID, MMI) |> 
+  rename(act_id = SAMPLEID)
+
+
+## Put it all together ---------------------------------------------------------------------------------------------
+
+
+OE <- OE_scores |> 
+  #select(MLocID,org_id, AU_ID, act_id,EcoRegion3,EcoRegion4,ReferenceSite, OoverE) |> 
+  mutate(BCG_region = case_when(EcoRegion3 %in% c(1,3,4) ~ "In region",
+                               TRUE ~ "Out Region"))
+
+BCG <- BCG_results |> 
+  select(SampleID, Primary_BCG_Level, Continuous_BCG_Level) |> 
+  rename(act_id = SampleID)
+
+
+# MMI <- MMI_scores |> 
+#   select(SAMPLEID, MMI) |> 
+#   rename(act_id = SAMPLEID)
+# 
+# MMI_met <- MMI_metrics |> 
+#    rename(act_id = SAMPLEID)
+
+joined_OE_BCG_MMI <- left_join(OE, BCG, by = join_by(act_id)) |> 
+  #left_join(MMI_met) |> 
+  left_join(MMI) |> 
+  filter(!is.na(MMI))
+  #filter(BCG_region == "In region")
+
+save(joined_OE_BCG_MMI, file = 'bioassess_6-28-24.Rdata')
+write.xlsx(joined_OE_BCG_MMI, file = paste0("biocriteria_scores", Sys.Date(), '.xlsx'))
+
+## Graphs ----------------------------------------------------------------------------------------------------------
+
+p1 <- ggplot(data = joined_OE_BCG_MMI, aes(x = Continuous_BCG_Level, y = OoverE))+
+  geom_point(aes(color = ReferenceSite))+
+  #stat_smooth(method=lm)+
+  geom_hline(yintercept = 0.8, color = 'red')+
+  geom_vline(xintercept = 3.5, color = "purple")+
+  geom_smooth(#data = filter(joined_OE_BCG_MMI, ReferenceSite == 'REFERENCE' ),  
+              method='lm', formula= y~x)+
+  scale_color_hue(l=50)+
+  labs(title = 'O:E + BCG')+
+  theme_bw()
+
+
+
+p2 <- ggplot(data = joined_OE_BCG_MMI, aes(x = MMI, y = OoverE))+
+  geom_point(aes(color = ReferenceSite))+
+  #stat_smooth(method=lm)+
+  geom_hline(yintercept = 0.8, color = 'red')+
+  geom_vline(xintercept = 0.55, color = "forestgreen")+
+  geom_smooth(#data = filter(joined_OE_BCG_MMI, ReferenceSite == 'REFERENCE' ), 
+    method='lm')+
+  scale_color_hue(l=50)+
+  labs(title = 'O:E + MMI',
+       subtitle = "SH generated MMI")+
+  theme_bw()
+
+
+p3 <- ggplot(data = joined_OE_BCG_MMI, aes(x = Continuous_BCG_Level, y = MMI))+
+  geom_point(aes(color = ReferenceSite))+
+  #stat_smooth(method=lm)+
+  geom_hline(yintercept = 0.55, color = 'forestgreen')+
+  geom_vline(xintercept = 3.5, color = "purple")+
+  geom_smooth(#data = filter(joined_OE_BCG_MMI, ReferenceSite == 'REFERENCE' ), 
+    method='lm', formula= y~x)+
+  scale_color_hue(l=50)+
+  labs(title = 'BCG + MMI',
+       subtitle = "SH generated MMI")+
+  theme_bw()
+
+###
+p5 <- ggplot(data =  filter(joined_OE_BCG_MMI,!is.na(Primary_BCG_Level) ), aes(y = OoverE, x = as.factor(Primary_BCG_Level)))+
+  geom_boxplot()+
+  theme_bw()+
+  geom_jitter(width = 0.2, alpha = .3)+
+  labs(title = 'O:E + BCG',
+       x = "Primary BCG")+
+  theme_bw()
+
+
+
+p4 <- ggplot(data =  filter(joined_OE_BCG_MMI,!is.na(Primary_BCG_Level) ), aes(y = MMI, x = as.factor(Primary_BCG_Level)))+
+  geom_boxplot()+
+  theme_bw()+
+  geom_jitter(width = 0.2, alpha = .3)+
+  labs(title = 'MMI + BCG',
+       x = "Primary BCG")
+
+
+#arrange
+library(patchwork)
+
+(p1 | p2 | p3) / (p5 | p4) + plot_layout(guides = "collect")
+
+
+library(openxlsx)
+
+
+# 3D --------------------------------------------------------------------------------------------------------------
+
+
+library(plotly) 
+
+
+
+
+data3d <- joined_OE_BCG_MMI |> 
+  filter(ReferenceSite == 'REFERENCE')
+
+fig <- plot_ly(data3d, x = ~OoverE, y = ~Continuous_BCG_Level, z = ~MMI, color = ~ReferenceSite)
+fig <- fig %>% add_markers()
+fig <- fig %>% layout(scene = list(xaxis = list(title = 'OoverE'),
+                                   yaxis = list(title = 'BCG'),
+                                   zaxis = list(title = 'MMI')),
+                      title = 'x=OE y = BCG z = MMI')
+
+fig
+
+htmlwidgets::saveWidget(as_widget(fig), "bugs analyses/All_together/bioassess_comp.html")
+
+
+
+
+
+
+
+
+# Using SH MMI ----------------------------------------------------------------------------------------------------
+
+
+
+#Everything below here is old
+# comparison ------------------------------------------------------------------------------------------------------
+
+
+# 
+# 
+# 
+# OE <- OE_scores |> 
+#   select(MLocID,org_id, AU_ID, act_id,EcoRegion3,EcoRegion4,ReferenceSite, OoverE) |> 
+#   mutate(ecoregion = case_when(EcoRegion3 %in% c(1,3,4) ~ "In region",
+#                                TRUE ~ "Out Region"))
+# 
+# BCG <- BCG_results |> 
+#   select(SampleID, Primary_BCG_Level, Continuous_BCG_Level) |> 
+#   rename(act_id = SampleID)
+# 
+# 
+# MMI <- MMI_scores |> 
+#   select(SAMPLEID, MMI) |> 
+#   rename(act_id = SAMPLEID)
+# 
+# joined_OE_BCG_MMI <- left_join(OE, BCG, by = join_by(act_id)) |> 
+#   left_join(MMI)
+# 
+# #  joined_OE_BCG_MMI <- joined_OE_BCG_MMI |> 
+# # #filter(!org_id %in% c('USU(NOSTORETID)', 'CAFW(NOSTORETID)')) |> 
+# #   filter(ecoregion == "In region" )
+# 
+# 
+# DEQ_data <- joined_OE_BCG_MMI |> 
+#   filter(org_id == 'OREGONDEQ')
+# 
+# DEQ_data <- joined_OE_BCG_MMI |> 
+#   filter(ReferenceSite != 'REFERENCE' )
+# 
+# 
+# ggplot(data = DEQ_data, aes(x = Continuous_BCG_Level, y = OoverE))+
+#   geom_point(aes(color = ReferenceSite))+
+#   #stat_smooth(method=lm)+
+#   geom_hline(yintercept = 0.8, color = 'red')+
+#   geom_vline(xintercept = 3.5, color = "forestgreen")+
+#   geom_smooth(data = filter(joined_OE_BCG_MMI, ReferenceSite != 'REFERENCE' ), 
+#               method='lm', formula= y~x)+
+#   scale_color_hue(l=50)
+# 
+# 
+# ggplot(data = joined_OE_BCG_MMI, aes(x = Continuous_BCG_Level, y = OoverE))+
+#   geom_point(aes(color = org_id))+
+#   #stat_smooth(method=lm)+
+#   geom_hline(yintercept = 0.8, color = 'red')+
+#   geom_smooth(method='lm', formula= y~x)+
+#   scale_color_hue(l=50)
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# joined_OE_BCG_MMI <- na.omit(joined_OE_BCG_MMI)
+# ggplot(data = joined_OE_BCG_MMI, aes(y = OoverE, x = as.factor(Primary_BCG_Level)))+
+#   geom_boxplot()+
+#   theme_bw()+
+# geom_jitter(width = 0.2, alpha = .3)
+# 
+# 
+# 
+# 
+# # MMI comp --------------------------------------------------------------------------------------------------------
+# 
+# 
+# ggplot(data = joined_OE_BCG_MMI, aes(x = MMI, y = OoverE))+
+#   geom_point(aes(color = ReferenceSite))+
+#   #stat_smooth(method=lm)+
+#   geom_hline(yintercept = 0.8, color = 'red')+
+#   #geom_vline(xintercept = 3.5, color = "forestgreen")+
+#   geom_smooth(#data = filter(joined_OE_BCG_MMI, ReferenceSite != 'REFERENCE' ), 
+#               method='lm', formula= y~x)+
+#   scale_color_hue(l=50)
+# 
+# 
+# 
+# ggplot(data = joined_OE_BCG_MMI, aes(x = Continuous_BCG_Level, y = MMI))+
+#   geom_point(aes(color = ReferenceSite))+
+#   #stat_smooth(method=lm)+
+#   #geom_hline(yintercept = 0.8, color = 'red')+
+#   #geom_vline(xintercept = 3.5, color = "forestgreen")+
+#   geom_smooth(#data = filter(joined_OE_BCG_MMI, ReferenceSite != 'REFERENCE' ), 
+#     method='lm', formula= y~x)+
+#   scale_color_hue(l=50)
+# 
+# 
+# 
+# 
+# 
+# ggplot(data =  na.omit(joined_OE_BCG_MMI), aes(y = MMI, x = as.factor(Primary_BCG_Level)))+
+#   geom_boxplot()+
+#   theme_bw()+
+#   geom_jitter(width = 0.2, alpha = .3)
+# 
+# 
+# # Missing NHD info ------------------------------------------------------------------------------------------------
+# 
+# no_NHD_acts <- BCG_metrics |> 
+#   filter(is.na(SITE_TYPE)) |> 
+#   select(SAMPLEID) |> 
+#   pull()
+# 
+# 
+# no_NHD <- sample_info |> 
+#   filter(act_id %in% no_NHD_acts) |> 
+#   filter(!is.na(COMID)) |> 
+#   get_NHD_info()
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# # 3D --------------------------------------------------------------------------------------------------------------
+# 
+# 
+# library(plotly) 
+# 
+# 
+# 
+# 
+# 
+# 
+# fig <- plot_ly(joined_OE_BCG_MMI, x = ~OoverE, y = ~Continuous_BCG_Level, z = ~MMI, color = ~ReferenceSite)
+# fig <- fig %>% add_markers()
+# fig <- fig %>% layout(scene = list(xaxis = list(title = 'OoverE'),
+#                                    yaxis = list(title = 'BCG'),
+#                                    zaxis = list(title = 'MMI')),
+#                       title = 'x=OE y = BCG z = MMI')
+# 
+# fig
+# 
+# htmlwidgets::saveWidget(as_widget(fig), "bioassess_comp.html")
--- bugs analyses/All_together/multirun.R
+++ bugs analyses/All_together/multirun.R
@@ -3,7 +3,7 @@
 
 
 
-num_runs <- 10
+num_runs <- 20
 
 # OE_multirun -----------------------------------------------------------------------------------------------------
 
@@ -29,22 +29,91 @@ oe_multi <- function(){
 }
 
 
-result_list <- purrr::map(1:num_runs, ~ oe_multi())
+result_list_OE <- purrr::map(1:num_runs, ~ oe_multi())
 
 
 # View(result_list[[1]])
 # View(result_list[[2]])
 
-result_stats <- data.frame(sample = result_list[[1]]$act_id,
-                     mean_indivduals = rowMeans(sapply(result_list, function(df) df$num_indiv)),
-                     sd_ind = apply(sapply(result_list, function(df) df$num_indiv), 1, sd),
-                     mean_num_taxa =  rowMeans(sapply(result_list, function(df) df$num_taxa)),
-                     sd_taxa = apply(sapply(result_list, function(df) df$num_taxa), 1, sd),
-                     mean_OE = rowMeans(sapply(result_list, function(df) df$OoverE)),
-                     sd_OE = apply(sapply(result_list, function(df) df$OoverE), 1, sd),
-                     var_OE = apply(sapply(result_list, function(df) df$OoverE), 1, var))
+result_stats_OE <- data.frame(sample = result_list_OE[[1]]$act_id,
+                     mean_indivduals = rowMeans(sapply(result_list_OE, function(df) df$num_indiv)),
+                     sd_ind = apply(sapply(result_list_OE, function(df) df$num_indiv), 1, sd),
+                     mean_num_taxa =  rowMeans(sapply(result_list_OE, function(df) df$num_taxa)),
+                     sd_taxa = apply(sapply(result_list_OE, function(df) df$num_taxa), 1, sd),
+                     mean_OE = rowMeans(sapply(result_list_OE, function(df) df$OoverE)),
+                     sd_OE = apply(sapply(result_list_OE, function(df) df$OoverE), 1, sd),
+                     var_OE = apply(sapply(result_list_OE, function(df) df$OoverE), 1, var))
 
 
 
 ggplot(result_stats, aes(x=sd_OE)) + geom_histogram()
+
+
+
+# MMI multirun ----------------------------------------------------------------------------------------------------
+
+
+MMI_multi <- function(){
+  
+  rand_subsample <- random_subsample(bug_tax_data_filtered)
+  
+  rand_stats <- rand_subsample |> 
+    group_by(Sample) |> 
+    summarise(num_taxa = n_distinct(OTU),
+              num_indiv = sum(Count))
+  
+  MMI_results <- MMI_run(rand_subsample, sample_info)
+  
+  MMI_scores <- MMI_results$MMI_result |> 
+    left_join(rand_stats, by = c('SAMPLEID' = 'Sample')) |> 
+    dplyr::arrange(SAMPLEID)
+  
+  
+
+  
+  return(MMI_scores)
+}
+
+
+result_list_MMI <- purrr::map(1:num_runs, ~ MMI_multi())
+
+
+result_stats_MMI <- data.frame(sample = result_list_MMI[[1]]$SAMPLEID,
+                           mean_indivduals = rowMeans(sapply(result_list_MMI, function(df) df$num_indiv)),
+                           sd_ind = apply(sapply(result_list_MMI, function(df) df$num_indiv), 1, sd),
+                           mean_num_taxa =  rowMeans(sapply(result_list_MMI, function(df) df$num_taxa)),
+                           sd_taxa = apply(sapply(result_list_MMI, function(df) df$num_taxa), 1, sd),
+                           mean_MMI = rowMeans(sapply(result_list_MMI, function(df) df$MMI)),
+                           sd_MMI = apply(sapply(result_list_MMI, function(df) df$MMI), 1, sd),
+                           var_MMI = apply(sapply(result_list_MMI, function(df) df$MMI), 1, var))
+
+ggplot(result_stats_OE, aes(x=sd_OE)) + 
+  geom_histogram() +
+  labs(title = paste("Histogram of O:E standard deviation over",num_runs, "sampling runs" ))+
+  ylab("Number of samples")+
+  theme_minimal()
+
+
+ggplot(result_stats_MMI, aes(x=sd_MMI)) + 
+  geom_histogram() +
+  labs(title = paste("Histogram of MMI standard deviation over",num_runs, "subsampling runs" ))+
+  ylab("Number of samples")+
+  theme_minimal()
+  
+results_together <- result_stats_OE |> 
+  select(sample, sd_OE) |> 
+  left_join( select(result_stats_MMI, sample, sd_MMI)) |> 
+  pivot_longer(cols = c(sd_OE, sd_MMI),
+               names_to = 'type',
+               values_to = 'st_dev')
+
+
+
+ggplot(results_together, aes(x=st_dev, fill = type, color=type)) + 
+  geom_histogram(alpha = 0.5,
+                 aes(x=st_dev, fill = type, color=type),
+                 position='identity') +
+  labs(title = paste("Histogram of standard deviation over",num_runs, "subsampling runs" ))+
+  ylab("Number of samples")+
+  theme_minimal()
 
